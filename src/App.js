34adb13ed3ea62620186bcdc74f4b27b6e97e3b8
ao <aolin@ymail.com>
2020-12-29 18:00:40 +0200

    six pm submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from "react"
 import HomePage from './components/HomePage';
+import Submissions from './components/Submission';
 import "./App.css";
 
 function App() {
