dbb4ef683539fa3fee4ac1f62533d4b267001caf
ao <aolin@ymail.com>
2020-12-30 11:56:27 +0200

    missing pics

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -10,38 +10,41 @@ function Submission() {
                 <div className="panel">
                     <h1 className="header">Submit your music</h1>
                     <div className="fieldWrap">
-                        <label for="FullName">Full name</label>
                         <input name="fullName" type="text" placeholder="Full name" />
+                        <label for="FullName">Full name</label>
                     </div>
                     <div className="fieldWrap">
-                        <label for="email">Email</label>
                         <input name="email" type="text" placeholder="Email" />
+                        <label for="email">Email</label>
                     </div>
                     <div className="fieldWrap">
-                        <label for="country">Country</label>
                         <input name="country" type="text" placeholder="Country" />
+                        <label for="country">Country</label>
+                    </div>
+                    <div className="tooltip">
+                        <p className="bio">Let us know who you are and where you’re coming from, what inspires you, who you have worked with and what you think is interesting about your work in the world of music and film, etc.  How should we present you as an artist? </p>
                     </div>
                     <div className="fieldWrap">
+                        <textarea placeholder="Bio" name="bio"></textarea>
                         <label for="bio">Bio</label>
-                        <input name="bio" type="textarea" placeholder="Bio" />
                     </div>
                     <div className="fieldWrap">
-                        <label for="job">What Do You Do?</label>
                         <input name="job" type="text" placeholder="What Do You Do?" />
+                        <label for="job">What Do You Do?</label>
                     </div>
                     <div className="fieldWrap">
-                        <label for="portfolio">Portfolio links</label>
                         <input name="portfolio" type="text" placeholder="portfolio links" />
+                        <label for="portfolio">Portfolio links</label>
                     </div>
                     <div className="fieldWrap radioBtn">
                         <p for="pro">Are you registered with a PRO?</p>
                         <span className="radioSpan">
-                            <input type="radio" id="pro" name="pro" value="yes" />
                             <label for="yes">Yes</label>
+                            <input type="radio" id="pro" name="pro" value="yes" />
                         </span>
                         <span className="radioSpan">
-                            <input type="radio" id="nopro" name="pro" value="no" />
                             <label for="nopro">No</label>
+                            <input type="radio" id="nopro" name="pro" value="no" />
                         </span>
                     </div>
                 </div>
